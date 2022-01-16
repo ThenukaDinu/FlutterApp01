2ac859feacb8e73aa9fab034b0999dfe8204cc6f
Thenukad Dinu <thenukadinu@gmail.com>
2022-01-17 01:05:56 +0530

    changes...

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
@@ -18,20 +19,41 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.red[600],
         ),
-        body: Center(
-          child: Text(
-            'Hello Ninjas!',
-            style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.grey[600],
-                fontFamily: 'IndieFlower'),
-          ),
+        body: Row(
+          children: [
+            Expanded(
+              child: Image.asset('assets/space-2.jpeg'),
+              flex: 3,
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.cyan,
+                child: Text('1'),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.pink,
+                child: Text('2'),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.amber,
+                child: Text('3'),
+              ),
+            )
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           child: Text('click'),
-          onPressed: () => {},
+          onPressed: () {},
           backgroundColor: Colors.red[600],
         ),
       ),
